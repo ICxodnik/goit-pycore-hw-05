9b52eb3ad72097521e4030cb6ce3b732e3ead13b
Natalia <icxodnik@gmail.com>
2025-03-31 06:51:33 +0200

    add second task

---
+++ tasks/task_2/task_2.py
@@ -0,0 +1,21 @@
+import re
+from typing import Callable
+
+
+def generator_numbers(text: str):
+    for match in find_floats(text):
+        yield float(match)
+
+def sum_profit(text: str, func: Callable):
+    return sum(func(text))
+
+
+def find_floats(text):
+    pattern = r'-?\d+\.\d+|-?\d+'  # Шукає дійсні та цілі числа
+    return re.findall(pattern, text)
+
+
+if __name__ == "__main__":
+    text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+    total_income = sum_profit(text, generator_numbers)
+    print(f"Загальний дохід: {total_income}")
